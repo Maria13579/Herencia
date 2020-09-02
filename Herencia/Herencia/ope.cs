d4044f6b9707182548f774a286b18a88e1b8b050
María Patricia Serrano Ramírez <201800378@colsanjose.edu.gt>
2020-09-02 09:00:14 -0600

    Creacion del Proyecto

---
+++ Herencia/Herencia/ope.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace herencia
+{
+    class ope
+    {
+        public double Suma(double a, double b)
+        {
+            return (a + b);
+        }
+        public double Resta(double a, double b)
+        {
+            return (a - b);
+        }
+        public double Multi(double a, double b)
+        {
+            return (a * b);
+        }
+        public double DIVI(double a, double b)
+        {
+            return (a / b);
+        }
+    }
+}
